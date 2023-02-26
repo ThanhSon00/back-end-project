e81f4d0f0d972e94b21ba7d820bb54a71174667f
ThanhSon00 <phanson999999@gmail.com>
2023-02-26 16:14:55 +0700

    Modify app and create www file

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require('express');
-const app = express();
+var app = express();
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var createError = require('http-errors');
+
 const accountRoutes = require('./route/accountRoutes');
 const customerRoutes = require('./route/customerRoutes');
 const cartRoutes = require('./route/cartRoutes');
 const productRoutes = require('./route/productRoutes');
 const invoiceRoutes = require('./route/invoiceRoutes');
+
+const errorHandler = require('./middleware/errorHandler');
+const asyncHandler = require('./middleware/asyncHandler');
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+// api
+app.use('/api/v1/account', asyncHandler(accountRoutes));
+app.use('/api/v1/customer', asyncHandler(customerRoutes));
+app.use('/api/v1/cart', asyncHandler(cartRoutes));
+app.use('/api/v1/product', asyncHandler(productRoutes));
+app.use('/api/v1/invoice', asyncHandler(invoiceRoutes));
+
+// Create Error 404 when not found path
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.use('/api/v1/account', accountRoutes);
-app.use('/api/v1/customer', customerRoutes);
-app.use('/api/v1/cart', cartRoutes);
-app.use('/api/v1/product', productRoutes);
-app.use('/api/v1/invoice', invoiceRoutes);
+// Middleware
+app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+module.exports = app;
