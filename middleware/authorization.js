821fe1f5a18dbe8d98ea30e21dc9e2a9b9b01023
ThanhSon00 <phanson999999@gmail.com>
2023-03-05 23:47:18 +0700

    Change variable name

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,12 +1,12 @@
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
 const authorization = async (req, res, next) => {
-    const access_token = req.cookies.access_token;
-    if (!access_token) {
+    const accessToken = req.cookies.access_token;
+    if (!accessToken) {
         return res.status(StatusCodes.UNAUTHORIZED).redirect('log-in'); 
     }
     try {
-        const data = jwt.verify(access_token, process.env.JWT_SECRET_KEY);
+        const data = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
         req.body.customer_id = data.customer_id;
         req.body.email = data.email;
     } catch (error) {
