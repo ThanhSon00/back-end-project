c01cf74bec4b2a1bc073544c8b62907871400d34
ThanhSon00 <phanson999999@gmail.com>
2023-03-19 23:23:21 +0700

    Create function handle add/remove product request

--- controllers/store.controller.js
+++ controllers/store.controller.js
@@ -1,3 +1,5 @@
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
 const { api } = require('../bin/URL');
 
 const renderPage = async (req, res) => {
@@ -9,29 +11,26 @@ const renderPage = async (req, res) => {
     const minPrice = req.query.price[0] || '';
     const maxPrice = req.query.price[1] || '';
     const page = req.query.page || 1;
-
     const response = await api.get(`/products?page=${page}&pageSize=${pageSize}&category_id=${category_id}&price=${minPrice}&price=${maxPrice}`)
     const { rows, count } = response.data;
-    const response1 = await api.get('categories');
-    const totalPage = Math.floor(count / pageSize) + 1;
-    
+    const categories = (await api.get('categories')).data;
+    const totalPage = Math.floor(count / pageSize) + 1;     
+    const customer = await getCurrentCustomer(req.cookies.access_token);
+    const cart = (await getRelatedResource(customer, 'cart'))
+    const cartProducts = (await getRelatedResource(cart, 'product')).Products;
     let relativePath = '/store?';
     if (category_id) {
         relativePath += `&category_id=${category_id}`;
     }
-    
     if (minPrice) {
         relativePath += `&price=${minPrice}`;
     }
-    
     if (maxPrice) {
         relativePath += `&price=${maxPrice}`;
     }
-
     if (page) {
         relativePath += `&page=${page}`;
-    }
-    
+    }    
     const pageFilteringPath = relativePath.replace(`&page=${page}`, '');
     const categoryFilteringPath = relativePath
         .replace(`&page=${page}`,'')
@@ -40,15 +39,15 @@ const renderPage = async (req, res) => {
         .replace(`&page=${page}`,'')
         .replace(`&price=${minPrice}`, '')
         .replace(`&price=${maxPrice}`, '');
-
+        
     return res.render('store', {
         products: rows,
         prevPageNumber: page - 1,
         activePageNumber: page,
         nextPageNumber: parseInt(page) + 1,
         productAmount: count,
-        totalPage: totalPage,
-        categories: response1.data,
+        totalPage,
+        categories,
         relativePath,
         category_id,
         minPrice,
@@ -56,9 +55,46 @@ const renderPage = async (req, res) => {
         pageFilteringPath,
         categoryFilteringPath,
         priceFilteringPath,
+        cart,
+        cartProducts,
+        api,
     });
+}
+
+const getCurrentCustomer = async (token) => {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const customer_id = data.customer_id; 
+    const response = await api.get(`/customers/${customer_id}`);
+    return response.data;
+}
+
+const getRelatedResource = async (parentResource, rel) => {
+    const link = parentResource._links.find((link) => link.rel == rel);
+    const response = await api({
+        method: link.method,
+        url: link.href,
+    })
+    return response.data;
+}
+
+const addProductToCart = async (req, res) => {
+    const data = jwt.verify(req.cookies.access_token, process.env.JWT_SECRET_KEY);
+    const customer_id = data.customer_id;
+    const cart = (await api.get(`/customers/${customer_id}/carts`)).data;
+    const cart_id = cart.cart_id;
+    const { product_id } = req.body;
+    const amount = req.body.amount || 1;
+    // Api need 2 keys which are customer_id and cart_id
+    const cartProduct = await api.post(`/carts/${cart_id}/products`, {
+        cart_id: cart_id,
+        product_id: product_id,
+        amount: amount,    
+    });
+    const cartProductObject = JSON.stringify(cartProduct.data);
+    res.status(StatusCodes.OK).json(cartProductObject);
 }
 
 module.exports = {
     renderPage,
+    addProductToCart
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,5 @@
+
+
 (function($) {
 	"use strict"
 
@@ -169,7 +171,7 @@
 
 // Prevent loading page when submit form
 $(document).ready(function(){
-	$('form').submit(function(event){
+	$('form#add-to-cart').submit(function(event){
 	  event.preventDefault(); // Prevent default form submission behavior
 	  var formData = $(this).serialize(); // Serialize form data
 	  $.ajax({
@@ -179,6 +181,8 @@ $(document).ready(function(){
 		success: function(response){
 		  // Handle success response
 		  showSuccessMessage();
+		  const product_id = this.data.replace('product_id=', '');
+		  addProductToCart(product_id);
 		},
 		error: function(xhr, status, error){
 		  // Handle error response
@@ -186,13 +190,31 @@ $(document).ready(function(){
 		}
 	  });
 	});
+	$('form.remove-from-cart').submit(function(event){
+		event.preventDefault(); // Prevent default form submission behavior
+		var formData = $(this).serialize(); // Serialize form data
+		$.ajax({
+		  url: $(this).attr('action'),
+		  type: $(this).attr('method'),
+		  data: formData,
+		  success: function(response){
+			// Handle success response
+			const product_id = this.data.replace('product_id=', '');
+			var div = document.querySelector(`form#${product_id}`);
+			div.remove();
+			decreaseTotalAmount();
+		  },
+		  error: function(xhr, status, error){
+			// Handle error response
+			console.log(xhr.responseText);
+		  }
+		});
+	  });
   });
   function showSuccessMessage() {
 	  var alert_items = document.querySelectorAll(".alert_item");
 	  var alert_wrapper = document.querySelector(".alert_wrapper");
 	  var close_btn = document.querySelectorAll(".close");
-	  // var message = "<%=locals.message%>";
-	  // var messageType = "<%=locals.messageType%>";
 	  alert_wrapper.classList.add("active");
 	  alert_items.forEach(function (alert_item, alert_index) {
 		  alert_item.style.top = "-100%";
@@ -211,4 +233,67 @@ $(document).ready(function(){
 	  document.querySelector(".alert_item.alert_success").style.top = "10%";
 	  document.querySelector(".alert_item.alert_success div.data p.sub").innerHTML = "Add product to cart successfully";
   }
+
+  function addProductToCart(product_id) {
+	const cartProduct = {
+		product_id: product_id,
+		image: document.querySelector(`div#${product_id} div.product-img div.cover img`).src,
+		name: document.querySelector(`div#${product_id} h3.product-name`).innerText,
+		price: document.querySelector(`div#${product_id} h4.product-price`).innerText.replace('$', ''),
+	};
+	const ejsView = `
+<div class="product-widget">
+	<form class="remove-from-cart" id="<%= cartProduct.product_id %>" method="delete" action="header">
+	  <input type="hidden" value="<%= cartProduct.product_id %>" name="product_id">
+	  <div class="product-img">
+		  <img src="<%= cartProduct.image %>" alt="">
+	  </div>
+	  <div class="product-body">
+		  <h3 class="product-name"><a href="#"><%= cartProduct.name %></a></h3>
+		  <h4 class="product-price"><span class="qty">1x</span>$<%= cartProduct.price %></h4>
+	  </div>
+	  <button class="delete" type="submit"><i class="fa fa-close"></i></button>    
+	</form>                                
+</div>`;
+	let html = ejs.render(ejsView, { cartProduct: cartProduct });
+	  // Vanilla JS:
+	const fragment = create(html);
+	// document.querySelector('.cart-items').appendChild(fragment);
+	document.querySelector('.cart-items').appendChild(fragment);
+	document.querySelector(`form#${product_id} button`).addEventListener('click', (event) => {
+		event.preventDefault(); // Prevent default form submission behavior
+		$.ajax({
+			url: 'header',
+			type: 'delete',
+			data: `product_id=${product_id}`,
+			success: function(response){
+			var div = document.querySelector(`form#${product_id}`);
+			div.remove();
+			decreaseTotalAmount();
+			},
+			error: function(xhr, status, error){
+			// Handle error response
+			console.log(xhr.responseText);
+			}
+		});
+	});
+	const totalAmount = document.querySelector('div.dropdown div.qty').innerText;
+	document.querySelector('div.dropdown div.qty').innerText = parseInt(totalAmount) + 1;
+
+  }
+  
+  function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+function decreaseTotalAmount () {
+	const totalAmount = document.querySelector('div.dropdown div.qty').innerText;
+	document.querySelector('div.dropdown div.qty').innerText = parseInt(totalAmount) - 1;
+}
 
