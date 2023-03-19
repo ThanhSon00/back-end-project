53bfe5d0bef67a51d66aefd4804812e5a22931b5
ThanhSon00 <phanson999999@gmail.com>
2023-03-19 23:12:17 +0700

    Implement resources link in customer method

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,4 +1,4 @@
-const Customer = require('../models/customer.model');
+const { Customer, Cart } = require('../models/models');
 const { StatusCodes } = require('http-status-codes');
 
 const getAllCustomers = async (req, res) => {
@@ -24,7 +24,27 @@ const getCustomer = async (req, res) => {
             customer_id: customer_id,
         }
     });
-    res.status(StatusCodes.OK).json(customer);
+    const objData = JSON.parse(JSON.stringify(customer, null, 4));
+    objData._links = [];
+    objData._links.push(getCartLink(customer_id));
+    objData._links.push(getAccountLink(customer_id));
+    res.status(StatusCodes.OK).json(objData);
+}
+
+const getCartLink = (customer_id) => {
+    return {
+        rel: "cart",
+        href: `/customers/${customer_id}/carts`,
+        method: "GET"
+    }
+}
+
+const getAccountLink = (customer_id) => {
+    return {
+        rel: "account",
+        href: `/customers/${customer_id}/accounts`,
+        method: "GET",
+    }
 }
 
 const updateCustomer = async (req, res) => {
@@ -51,10 +71,32 @@ const deleteCustomer = async (req, res) => {
     res.status(StatusCodes.OK).send();
 }
 
+const getCustomerCart = async (req, res) => {
+    const { customer_id } = req.params;
+    const customerCart = await Cart.findOne({
+        where: {
+            customer_id: customer_id
+        }
+    })
+    const objData = JSON.parse(JSON.stringify(customerCart));
+    objData._links = [];
+    objData._links.push(getProductsLink(customerCart.cart_id));
+    res.status(StatusCodes.OK).json(objData);
+}
+
+const getProductsLink = (cart_id) => {
+    return {
+        rel: "product",
+        href: `carts/${cart_id}/products`,
+        method: "GET",
+    }
+}
+
 module.exports = {
     getAllCustomers,
     getCustomer,
     updateCustomer,
     createCustomer,
     deleteCustomer,
+    getCustomerCart,
 }
