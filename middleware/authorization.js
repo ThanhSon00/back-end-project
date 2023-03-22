06a619c6c06e8611a20c087e4c9ade8b473a9527
ThanhSon00 <phanson999999@gmail.com>
2023-03-22 17:46:37 +0700

    Change to root url for images

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,15 +1,17 @@
 const { StatusCodes } = require('http-status-codes');
 const jwt = require('jsonwebtoken');
+const { root } = require('../bin/URL');
+const rootURL = root.defaults.baseURL;
 const authorization = async (req, res, next) => {
     const accessToken = req.cookies.access_token;
     if (!accessToken) {
-        return res.status(StatusCodes.UNAUTHORIZED).redirect('log-in'); 
+        return res.status(StatusCodes.UNAUTHORIZED).redirect(`${rootURL}/log-in`); 
     }
 
     await jwt.verify(accessToken, process.env.JWT_SECRET_KEY, (err, data) => {
         if (err) {
             res.clearCookie('access_token');
-            return res.status(StatusCodes.FORBIDDEN).redirect('log-in');
+            return res.status(StatusCodes.FORBIDDEN).redirect(`${rootURL}/log-in`);
         }
         req.body.customer_id = data.customer_id;
         req.body.email = data.email;
