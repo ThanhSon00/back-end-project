421959cf3f4211e5bb757166b806b9b4b384658a
ThanhSon00 <phanson999999@gmail.com>
2023-03-23 17:50:20 +0700

    Add checkout controller and route

---
+++ routes/checkout.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    
+} = require('../controllers/checkout.controller');
+
+router.route('/').get()
+
+module.exports = router;
