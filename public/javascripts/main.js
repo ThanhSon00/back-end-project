b14d6d5f2297bd0fb33e24be423099b26d448fd9
ThanhSon00 <phanson999999@gmail.com>
2023-03-26 18:23:13 +0700

    set for "add to cart" method in product page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,7 +19,7 @@
 	// Products Slick
 	$('.products-slick').each(function() {
 		var $this = $(this),
-				$nav = $this.attr('data-nav');
+			$nav = $this.attr('data-nav');
 
 		$this.slick({
 			slidesToShow: 4,
@@ -31,27 +31,27 @@
 			arrows: true,
 			appendArrows: $nav ? $nav : false,
 			responsive: [{
-	        breakpoint: 991,
-	        settings: {
-	          slidesToShow: 2,
-	          slidesToScroll: 1,
-	        }
-	      },
-	      {
-	        breakpoint: 480,
-	        settings: {
-	          slidesToShow: 1,
-	          slidesToScroll: 1,
-	        }
-	      },
-	    ]
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+			]
 		});
 	});
 
 	// Products Widget Slick
 	$('.products-widget-slick').each(function() {
 		var $this = $(this),
-				$nav = $this.attr('data-nav');
+			$nav = $this.attr('data-nav');
 
 		$this.slick({
 			infinite: true,
@@ -67,34 +67,34 @@
 
 	// Product Main img Slick
 	$('#product-main-img').slick({
-    infinite: true,
-    speed: 300,
-    dots: false,
-    arrows: true,
-    fade: true,
-    asNavFor: '#product-imgs',
-  });
+		infinite: true,
+		speed: 300,
+		dots: false,
+		arrows: true,
+		fade: true,
+		asNavFor: '#product-imgs',
+	});
 
 	// Product imgs Slick
-  $('#product-imgs').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true,
-    centerMode: true,
-    focusOnSelect: true,
+	$('#product-imgs').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		centerMode: true,
+		focusOnSelect: true,
 		centerPadding: 0,
 		vertical: true,
-    asNavFor: '#product-main-img',
+		asNavFor: '#product-main-img',
 		responsive: [{
-        breakpoint: 991,
-        settings: {
-					vertical: false,
-					arrows: false,
-					dots: true,
-        }
-      },
-    ]
-  });
+			breakpoint: 991,
+			settings: {
+				vertical: false,
+				arrows: false,
+				dots: true,
+			}
+		},
+		]
+	});
 
 	// Product img zoom
 	var zoomMainProduct = document.getElementById('product-main-img');
@@ -105,44 +105,49 @@
 	/////////////////////////////////////////
 
 	// Input number
-	$('.input-number').each(function() {
+	$('.input-number').each(function () {
 		var $this = $(this),
-		$input = $this.find('input[type="number"]'),
-		up = $this.find('.qty-up'),
-		down = $this.find('.qty-down');
+			$input = $this.find('input[type="number"]'),
+			up = $this.find('.qty-up'),
+			down = $this.find('.qty-down'),
+			button = $('.product-details .add-to-cart-btn');
 
 		down.on('click', function () {
 			var value = parseInt($input.val()) - 1;
 			value = value < 1 ? 1 : value;
 			$input.val(value);
 			$input.change();
-			updatePriceSlider($this , value)
+			updatePriceSlider($this, value)
+			button.attr("data-amount", value);
 		})
 
 		up.on('click', function () {
 			var value = parseInt($input.val()) + 1;
 			$input.val(value);
 			$input.change();
-			updatePriceSlider($this , value)
+			updatePriceSlider($this, value)
+			button.attr("data-amount", value);
 		})
 	});
 
 	var priceInputMax = document.getElementById('price-max'),
-			priceInputMin = document.getElementById('price-min');
+		priceInputMin = document.getElementById('price-min');
 
-	priceInputMax.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
-	});
+	if (priceInputMax && priceInputMin) {
+		priceInputMax.addEventListener('change', function () {
+			updatePriceSlider($(this).parent(), this.value)
+		});
+	
+		priceInputMin.addEventListener('change', function () {
+			updatePriceSlider($(this).parent(), this.value)
+		});
+	}
 
-	priceInputMin.addEventListener('change', function(){
-		updatePriceSlider($(this).parent() , this.value)
-	});
-
-	function updatePriceSlider(elem , value) {
-		if ( elem.hasClass('price-min') ) {
+	function updatePriceSlider(elem, value) {
+		if (elem.hasClass('price-min')) {
 			console.log('min')
 			priceSlider.noUiSlider.set([value, null]);
-		} else if ( elem.hasClass('price-max')) {
+		} else if (elem.hasClass('price-max')) {
 			console.log('max')
 			priceSlider.noUiSlider.set([null, value]);
 		}
@@ -161,7 +166,7 @@
 			}
 		});
 
-		priceSlider.noUiSlider.on('update', function( values, handle ) {
+		priceSlider.noUiSlider.on('update', function (values, handle) {
 			var value = values[handle];
 			handle ? priceInputMax.value = value : priceInputMin.value = value
 		});
@@ -169,40 +174,54 @@
 
 })(jQuery);
 
-$(document).ready(function(){
-	$("button.delete").click(function() { 
+$(document).ready(function () {
+	$("button.delete").click(function () {
 		const cart_id = $(this).data("cart-id");
 		const product_id = $(this).data("product-id");
 		$.ajax({
 			url: `/api/v1/carts/${cart_id}/products/${product_id}`,
 			type: 'delete',
 			data: '',
-			success: function(response){
-			// Handle success response
-			var div = document.querySelector(`.product-widget#${product_id}`);
-			div.remove();
-			decreaseTotalAmount();
+			success: function (response) {
+				// Handle success response
+				var div = document.querySelector(`.product-widget#${product_id}`);
+				div.remove();
+				decreaseTotalAmount();
 			},
-			error: function(xhr, status, error){
-			// Handle error response
-			console.log(xhr.responseText);
+			error: function (xhr, status, error) {
+				// Handle error response
+				console.log(xhr.responseText);
 			}
 		});
 	});
-	$("button.add-to-cart-btn").click(function(e) {
+	$("button.add-to-cart-btn").click(function (e) {
+		const url = window.location.pathname;
+		var page;
+		if (url.includes('products')) {
+			page = "product";
+		}
+		else if (url.includes('store')) {
+			page = "store";
+		}
 		const cart_id = $(this).data("cart-id");
 		const product_id = $(this).data("product-id");
+		const amount = $(this).data("amount") || 1;
+		const json = {
+			"product_id": product_id,
+			"amount": amount,
+		}
 		$.ajax({
 			url: `/api/v1/carts/${cart_id}/products`,
 			type: 'post',
-			data: `product_id=${product_id}`,
-			success: function(response){
-			addProductToCart(cart_id, product_id);
-			showSuccessMessage();
+			data: json,
+			dataType: 'json',
+			success: function (response) {
+				addProductToCart(cart_id, product_id, page);
+				showSuccessMessage();
 			},
-			error: function(xhr, status, error){
-			// Handle error response
-			console.log(xhr.responseText);
+			error: function (xhr, status, error) {
+				// Handle error response
+				console.log(xhr.responseText);
 			}
 		});
 	});
@@ -232,13 +251,26 @@ function showSuccessMessage() {
 	document.querySelector(".alert_item.alert_success div.data p.sub").innerHTML = "Add product to cart successfully";
 }
 
-function addProductToCart(cart_id, product_id) {
-	const cartProduct = {
-		product_id: product_id,
-		image: document.querySelector(`div#${product_id} div.product-img div.cover img`).src,
-		name: document.querySelector(`div#${product_id} h3.product-name`).innerText,
-		price: document.querySelector(`div#${product_id} h4.product-price`).innerText.replace('$', ''),
-	};
+function addProductToCart(cart_id, product_id, page) {
+	let cartProduct = {};
+	if (page == "store") {
+		cartProduct = {
+			product_id: product_id,
+			image: document.querySelector(`div#${product_id} div.product-img div.cover img`).src,
+			name: document.querySelector(`div#${product_id} h3.product-name`).innerText,
+			price: document.querySelector(`div#${product_id} h4.product-price`).innerText.replace('$', ''),
+			amount: 1,
+		};
+	}
+	if (page == "product") {
+		cartProduct = {
+			product_id: product_id,
+			image: document.querySelector(".product-preview img").src,
+			name: document.querySelector("h2.product-name").textContent,
+			price: document.querySelector("h3.product-price").textContent.replace('$',''),
+			amount: document.querySelector('.input-number input').value,
+		}
+	}
 	const ejsView = `
 <div class="product-widget" id="<%= cartProduct.product_id %>">
 <div class="product-img">
@@ -248,7 +280,7 @@ function addProductToCart(cart_id, product_id) {
 </div>
 <div class="product-body">
 	<h3 class="product-name"><a href="#"><%= cartProduct.name %></a></h3>
-	<h4 class="product-price"><span class="qty">1x</span>$<%= cartProduct.price %></h4>
+	<h4 class="product-price"><span class="qty"><%= cartProduct.amount %>x</span>$<%= cartProduct.price %></h4>
 </div>
 <button class="delete" data-product-id="<%= cartProduct.product_id %>" data-cart-id="<%= cart_id %>"><i class="fa fa-close"></i></button>                              
 </div>`;
@@ -276,7 +308,6 @@ function addProductToCart(cart_id, product_id) {
 	});
 	const totalAmount = document.querySelector('div.dropdown div.qty').innerText;
 	document.querySelector('div.dropdown div.qty').innerText = parseInt(totalAmount) + 1;
-
 }
 
 function create(htmlStr) {
