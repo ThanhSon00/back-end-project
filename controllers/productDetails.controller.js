7a23c30afb48ce4cf141574619b718d4279ee27f
ThanhSon00 <phanson999999@gmail.com>
2023-03-26 18:19:46 +0700

    Add data for rendering page

--- controllers/productDetails.controller.js
+++ controllers/productDetails.controller.js
@@ -10,14 +10,28 @@ const {
 const renderPage = async (req, res) => {
     const { product_id } = req.params;
     const customer = await getCurrentCustomer(req.cookies.access_token);
-    const cart = (await getRelatedResource(customer, 'cart'))
-    const cartProducts = (await getRelatedResource(cart, 'product')).Products;
-    
+    const cart = await getRelatedResource(customer, 'cart')
+    const cartProducts = await getRelatedResource(cart, 'product');
+    const productDetails = (await api.get(`/products/${product_id}/details`)).data;
+    const productImages = productDetails.image.replaceAll('US40', 'SX420').split('|');
+    const product = (await api.get(`/products/${product_id}`)).data;
+    const random = randomIntFromInterval(1, 14);
+    const relatedProducts = (await api.get(`/products?category_id=${product.category_id}&pageSize=4&page=${random}`)).data;
+    const productCategory = (await api.get(`/products/${product_id}/categories`)).data;
     return res.render('product', {
         cart,
         cartProducts,
+        productDetails,
+        productImages,
+        product,
+        relatedProducts: relatedProducts.rows,
+        productCategory,
     });
 }
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 
 module.exports = {
     renderPage,
