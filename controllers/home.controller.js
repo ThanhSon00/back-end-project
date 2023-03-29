9755cc689f86507b924cae2a980d3b0ded2fb191
ThanhSon00 <phanson999999@gmail.com>
2023-03-29 18:05:38 +0700

    Add product Data to home page

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -5,8 +5,10 @@ const renderPage = async (req, res) => {
     const { cart_id } = data;
     const cart = (await api.get(`/carts/${cart_id}`)).data;
     const cartProducts = (await api.get(`/carts/${cart_id}/products`)).data;
+    const categories = (await api.get(`/categories`)).data;
     const message = req.cookies.message;
     const messageType = req.cookies.messageType;
+    const products = (await api.get(`/products?pageSize=150&page=${randomIntFromInterval(1, 5)}`)).data.rows;
     res.clearCookie('message');
     res.clearCookie('messageType');
     return res.render('index', {
@@ -14,7 +16,13 @@ const renderPage = async (req, res) => {
         messageType,
         cart,
         cartProducts,
+        categories,
+        products,
     });  
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 module.exports = {
