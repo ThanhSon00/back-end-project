887daa6e66f5f67a56673dbc3eb17c88604cc3d4
ThanhSon00 <phanson999999@gmail.com>
2023-03-30 18:11:25 +0700

    Fix image quality

--- controllers/productDetails.controller.js
+++ controllers/productDetails.controller.js
@@ -13,7 +13,7 @@ const renderPage = async (req, res) => {
     const cart = await getRelatedResource(customer, 'cart')
     const cartProducts = await getRelatedResource(cart, 'product');
     const productDetails = (await api.get(`/products/${product_id}/details`)).data;
-    const productImages = productDetails.image.replaceAll('US40', 'SX420').split('|');
+    const productImages = productDetails.image.replaceAll('US40', 'SX500').replaceAll('SX38_SY50_CR003850', 'SX500').split('|');
     const product = (await api.get(`/products/${product_id}`)).data;
     const random = randomIntFromInterval(1, 14);
     const relatedProducts = (await api.get(`/products?category_id=${product.category_id}&pageSize=4&page=${random}`)).data;
