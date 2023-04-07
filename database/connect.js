d993d406e7dda7d7f14b5a27757212a009248175
ThanhSon00 <phanson999999@gmail.com>
2023-04-07 14:16:53 +0700

    Change dialect

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('sql12611726', 'sql12611726', 'NkEtZBBmhR', {
   host: 'sql12.freemysqlhosting.net',
-  dialect: 'mysql'
+  dialect: require('mysql2'),
 });
 
 sequelize.authenticate()
