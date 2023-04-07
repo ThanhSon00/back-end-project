0895a0fffb2218bd403d5a76c4ba934e8b16853b
ThanhSon00 <phanson999999@gmail.com>
2023-04-07 13:15:03 +0700

    Redirect to another path

--- controllers/logout.controller.js
+++ controllers/logout.controller.js
@@ -1,25 +1,9 @@
 const { root, api } = require('../bin/URL');
 const { StatusCodes } = require('http-status-codes');
 const rootURL = root.defaults.baseURL;
-const jwt = require('jsonwebtoken');
-const moment = require('moment');
 
 const logout = async (req, res) => {
-    const ttl = process.env.REFRESH_TOKEN_TTL;
-    const refreshToken = req.cookies.refresh_token;
-    const data = parseJwt(refreshToken);
-    
-    await api.post(`/tokens/${data.customer_id}`, {
-        jti: data.jti,
-        expiration: moment(new Date()).add(parseInt(ttl.replace('m')), 'minutes').toDate().toISOString(),
-    })
-    res.clearCookie('access_token');
-    res.clearCookie('refresh_token');
-    res.status(StatusCodes.OK).redirect(rootURL + '/log-in');
-}
-
-const parseJwt = (token) => {
-    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+    res.redirect('/refresh-token/revoke');    
 }
 
 module.exports = {
